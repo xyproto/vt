34875563cf2102e5ec427e28bdd5b99af825a8a0
Alexander F. Rødseth <rodseth@gmail.com>
2025-06-26 19:33:09 +0200

    Update the ColorSplit function

--- colorsplit.go
+++ colorsplit.go
@@ -5,7 +5,7 @@ import "strings"
 // ColorSplit splits on the first sep in line. It returns two parts: left and right.
 // The right part includes the sep itself (so subsequent splits see it).
 // nil color funcs are skipped. reverse=true swaps which side gets the fallback when sep is absent.
-func ColorSplit(line, sep string, colorHead, colorSep, colorTail func(string) string, reverse bool) (string, string) {
+func ColorSplit(line, sep string, headColor, sepColor, tailColor AttributeColor, reverse bool) (string, string) {
 	if sep == "" {
 		if reverse {
 			return "", line
@@ -20,26 +20,40 @@ func ColorSplit(line, sep string, colorHead, colorSep, colorTail func(string) st
 		return line, ""
 	}
 	head := line[:idx]
-	sepAndTail := line[idx:] // includes sep
-	var left, right strings.Builder
-	// Head
-	if colorHead != nil {
-		left.WriteString(colorHead(head))
+	tail := line[idx+len(sep):]
+	var a, b string
+	if reverse {
+		if tailColor != 0 {
+			a = tailColor.Get(tail)
+		} else {
+			a = tail
+		}
+		if sepColor != 0 {
+			a = sepColor.Get(sep) + a
+		} else {
+			a = sep + a
+		}
+		if headColor != 0 {
+			b = headColor.Get(head)
+		} else {
+			b = head
+		}
 	} else {
-		left.WriteString(head)
+		if headColor != 0 {
+			a = headColor.Get(head)
+		} else {
+			a = head
+		}
+		if sepColor != 0 {
+			a += sepColor.Get(sep)
+		} else {
+			a += sep
+		}
+		if tailColor != 0 {
+			b = tailColor.Get(tail)
+		} else {
+			b = tail
+		}
 	}
-	// Separator
-	if colorSep != nil {
-		right.WriteString(colorSep(sep))
-	} else {
-		right.WriteString(sep)
-	}
-	// Tail
-	tail := sepAndTail[len(sep):]
-	if colorTail != nil {
-		right.WriteString(colorTail(tail))
-	} else {
-		right.WriteString(tail)
-	}
-	return left.String(), right.String()
+	return a, b
 }
